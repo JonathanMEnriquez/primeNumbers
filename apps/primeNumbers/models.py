d8dc01590d6e4989fd18b082775ebe6a7cedabd5
user <user@users-MacBook-Pro.local>
2018-03-22 21:18:14 -0700

    midway through with python

--- apps/primeNumbers/models.py
+++ apps/primeNumbers/models.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class PrimeManager(models.Manager):
+    def isValid(self, postData):
+        response = {}
+        try:
+            if int(postData) < 2:
+                response['error'] = []
+                response['error'].append("Number must be greater than 1")
+        except:
+            response['error'] = []
+            response['error'].append("Please enter valid data")
+        return response
+
+class Prime(models.Model):
+    objects = PrimeManager()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
--- apps/primeNumbers/views.py
+++ apps/primeNumbers/views.py
@@ -2,8 +2,28 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
 
 def index(request):
     return render(request, 'primeNumbers/index.html')
+
+def prime(request):
+    if request.method == 'POST':
+        error = Prime.objects.isValid(request.POST)
+        if len(error) > 0:
+            for message in error['error']:
+                messages.error(request, message)
+            return redirect('/')
+        else:
+            num = int(request.POST['max'])
+            print num
+            
+            # run the for loop
+            for i in 1 ... num :
+                print i
+    else:
+        return redirect('/')
+    return    
